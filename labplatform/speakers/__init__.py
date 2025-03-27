762912be21aea05f02ac1b98504203c2032b4360
ChaoHuang <c.huang03@gmail.com>
2025-03-27 18:02:00 +0100

    minor fix

--- labplatform/speakers/__init__.py
+++ labplatform/speakers/__init__.py
@@ -1,5 +1,5 @@
-from speaker_config import load_speaker_config, save_speaker_config, Speaker, SpeakerArray
-from utilities import get_sound_speed, get_system_delay, dB_to_Vrms, dBdiff_to_Vrmsratio, \
+from .speaker_config import load_speaker_config, save_speaker_config, Speaker, SpeakerArray
+from .utilities import get_sound_speed, get_system_delay, dB_to_Vrms, dBdiff_to_Vrmsratio, \
     Vrmsratio_to_dBdiff, Vrms_to_dB, filter_freq_response
-from calibrate import SpeakerCalibrator
+from .calibrate import SpeakerCalibrator
 
